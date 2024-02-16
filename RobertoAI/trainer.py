5abcf76bbeb289504607581c7eead4f541880598
yuvalprogramer@gmail.com <yuvalprogramer@gmail.com>
2024-02-16 15:17:21 +0200

    commit

--- RobertoAI/trainer.py
+++ RobertoAI/trainer.py
@@ -6,24 +6,10 @@ import utils
 from tqdm import tqdm
 
 
-def train_with_custom_data(Roberto: ChatBot, custom_data):
-    with tqdm(total=len(custom_data), desc='Training custom data') as pbar:
-        for item in custom_data:
-            # Extract relevant information from custom data
-            question = item.get('question')
-            answer = item.get('answer')
 
-            # Create statement objects
-            question_statement = Statement(text=question)
-            answer_statement = Statement(text=answer)
-
-            # Learn from the statement objects
-            Roberto.learn_response(answer_statement, question_statement)
-            pbar.update(1)  # Update progress bar
 
 
 def training(Roberto_ai: ChatBot):
     trainer = ChatterBotCorpusTrainer(Roberto_ai)
-    trainer.train('chatterbot.corpus.english', "C:/Users/yuval/PycharmProjects/chat-ai/knowledgeExamples.json")
-    custom_data = utils.load_custom_data("C:/Users/yuval/PycharmProjects/chat-ai/new-data.json")
-    train_with_custom_data(Roberto_ai, custom_data)
+    trainer.train('chatterbot.corpus.english', "C:/Users/yuval/PycharmProjects/chat-ai/DataSets/knowledgeExamples.json")
+
--- WebScraping/ScrapeInformation.py
+++ WebScraping/ScrapeInformation.py
@@ -1,20 +1,54 @@
+import nltk
 import praw
-from Settings import RedditSettings
 
-# Authenticate with Reddit API (replace placeholders with your actual credentials)
+from Settings import RedditSettings
+from rake_nltk import Rake
+
+# nltk.download('punkt')
+# nltk.download('stopwords')
+
+# Authenticate with Reddit API
 config = RedditSettings.get_config()
 reddit = praw.Reddit(client_id=config["id"],
                      client_secret=config["secret"],
                      user_agent=config["agent"])
 
 
-def casual_conversation():
-    # Specify the subreddit you want to scrape
-    subreddit_name = "CasualConversation"
-    subreddit = reddit.subreddit(subreddit_name).top(limit=10)
+def detect_subjects(text):
+    r = Rake()
+    r.extract_keywords_from_text(text)
+    for rating, keyword in r.get_ranked_phrases_with_scores():
+        if rating > 3:
+            print(rating, keyword)
 
-    # Stream all comments from the subreddit
-    for comment in subreddit.stream.comments():
-        print(comment.body)
 
-casual_conversation()
+def doctor_subjects_data():
+    target_subreddit = "AskDocs"
+
+    num_submissions = 10
+    data = []
+
+    for submission in reddit.subreddit(target_subreddit).new(limit=num_submissions):
+
+        subject = submission.title
+        comments = []
+        for comment in submission.comments:
+            comments.append(comment.body)
+
+        data.append({"subject": subject, "comments": comments})
+    print(data)
+
+
+def doctor_commadns_data():
+    target_subreddit = "AskDocs"
+
+    subreddit = reddit.subreddit(target_subreddit)
+    submission = subreddit.submission(id="SUBMISSION_ID")
+    comments = submission.comments.list()
+
+    for comment in comments:
+        if "keyword1" in comment.body or "keyword2" in comment.body:
+            print(comment.body)
+
+
+detect_subjects("hello doctor, i have a broken leg. last night i run into the streers and fall down and broke my leg.")
